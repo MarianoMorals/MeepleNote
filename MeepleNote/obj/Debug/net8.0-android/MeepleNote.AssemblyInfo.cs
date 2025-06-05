61436dcdd72d2191ac88d57a40c9f93969ad741a
MarianoMorals <145559141+MarianoMorals@users.noreply.github.com>
2025-06-05 20:37:14 +0200

    Guardado Seguridad

--- MeepleNote/obj/Debug/net8.0-android/MeepleNote.AssemblyInfo.cs
+++ MeepleNote/obj/Debug/net8.0-android/MeepleNote.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeepleNote")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e5d1bbab4b23d36a81fabaf9bed59271e4932ab1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8a449265d8ae6585431e5bff8b0f93037e0b3d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeepleNote")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeepleNote")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
